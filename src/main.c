2bf32fff94db0b976f78ff6ddad741b25e9e45c0
grish-ka <grishka.krot@gmail.com>
2024-12-31 07:40:18 +0000

    add lexer

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "include/lexer.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    
+    lexer_T* lexer = init_lexer(
+        "name = \"hello\";"
+    );
+
+    token_T* tokon = (void*)0;
+    int i = 0;
+
+    while ((tokon = lexer_get_next_token(lexer)) != (void*)0) {
+        printf("TOKEN(%d, %s)\n", tokon->type, tokon->value);
+        printf("i=%d\n", i);
+        i ++;
+
+    }
+    printf("I Am stuck\n");
+
     return 0;
 }
