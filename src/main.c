e37d6f9ae32cbf9c6940e223a13081c4f1d7a7bd
grish-ka <grishka.krot@gmail.com>
2024-12-31 07:48:41 +0000

    fix bug

--- src/main.c
+++ src/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include "include/lexer.h"
 
-int main(int argc, char* argv[]) {
-    
+
+int main(int argc, char* argv[])
+{
     lexer_T* lexer = init_lexer(
-        "name = \"hello\";"
+        "var name = \"john doe\";\n"
+        "print(name);\n"
     );
-
-    token_T* tokon = (void*)0;
-    int i = 0;
-
-    while ((tokon = lexer_get_next_token(lexer)) != (void*)0) {
-        printf("TOKEN(%d, %s)\n", tokon->type, tokon->value);
-        printf("i=%d\n", i);
-        i ++;
-
+    
+    token_T* token = (void*)0;
+    while ((token = lexer_get_next_token(lexer)) != (void*)0)
+    {
+        printf("TOKEN(%d, %s )\n", token->type, token->value);
     }
-    printf("I Am stuck\n");
 
     return 0;
 }
